e3018c2f9e9f102f9e2274e76f147af63f777d06
wuchen <897528943@qq.com>
2018-10-22 21:37:19 +0800

    第四次尝试

--- app/src/main/java/com/example/app/Appinitia.java
+++ app/src/main/java/com/example/app/Appinitia.java
@@ -12,6 +12,7 @@ public class Appinitia extends Application {
         super.onCreate();
         Initializer.init(this)
                .withApiHost("119.23.27.224")
+
                // .withApiHost("192.168.31.132")
                // .withApiHost("192.168.43.204")
                 .withPort(5255)
